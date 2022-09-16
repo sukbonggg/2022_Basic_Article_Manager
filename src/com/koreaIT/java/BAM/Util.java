e8197b10899335ba8b70a89710de5885d92b19b9
sukbonggg <kiti982611@gmail.com>
2022-09-16 21:55:50 +0900

    게시글 날짜 및시간,list detail 시간

---
+++ src/com/koreaIT/java/BAM/Util.java
@@ -0,0 +1,15 @@
+package com.koreaIT.java.BAM;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	// 현재 날짜와 시간 리턴
+	public static String getNowDateStr() {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Date now = new Date();
+		
+		return sdf1.format(now);
+	}
+}
